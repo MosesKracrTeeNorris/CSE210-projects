293d01b84b97870e8abf2214ffda92fcdf7703ce
MosesKracrTeeNorris <moseskracartee@hotmail.com>
2025-03-12 17:19:59 +0000

    edited exercise 3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,41 +1,35 @@
 using System;
 
-class Program
+Console.WriteLine("What is your grade percentage?");
+string answer = Console.ReadLine();
+int percent = int.Parse(answer);
+string letter = "";
+if (percent >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("What is your grade percentage?");
-        string answer = Console.ReadLine();
-        int percent = int.Parse(answer);
-        string letter = "";
-        if (percent >= 90)
-        {
-        letter = "A";
-        }
-        else if (percent >= 80)
-        {
-            letter = "B";
-        }
-        else if (percent >= 70)
-        {
-            letter = "C";
-        }
-        else if (percent >= 60)
-        {
-            letter = "D";
-        }
-        else
-        {
-            letter = "F";
-        }
-        Console.WriteLine($"Your grade is: {letter}");
-        if (percent >= 70)
-        {
-            Console.WriteLine("You passed!");
-        }
-        else
-        {
-            Console.WriteLine("Better Luck next time!");
-        }
-    }
+    letter = "A";
+}
+else if (percent >= 80)
+{
+    letter = "B";
+}
+else if (percent >= 70)
+{
+    letter = "C";
+}
+else if (percent >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+Console.WriteLine($"Your grade is: {letter}");
+if (percent >= 70)
+{
+    Console.WriteLine("You passed!");
+}
+else
+{
+    Console.WriteLine("Better Luck next time!");
 }
