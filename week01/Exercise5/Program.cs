cb81b0eb0f31af01ca5a10925e3efe95a7a5d129
MosesKracrTeeNorris <moseskracartee@hotmail.com>
2025-03-12 20:30:39 +0000

    Edited exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,3 +1,31 @@
 using System;
+class program
+{
+   static void Main(string[] args)
+   {
+    DisplayWelcomeMessage();
 
-Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    string userName = PromptUserName();
+    int squaredNumber = squaredNumber(userNumber);
+    DisplayResult(userName, squareNumber);
+
+   }
+   static void DisplayWelcomeMessage()
+   {
+    Console.WriteLine("Please enter your name:");
+    string name = Console.ReadLine();
+
+    return name;
+   }
+   static int SquareNumber(int number)
+   {
+    int square = number * number;
+    return square;
+   }
+   static void DisplayResult(string name, int square)
+   {
+
+       Console.WriteLine($"{name}, the square of your number is {square}");
+   }
+
+}
